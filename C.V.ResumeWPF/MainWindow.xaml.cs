fc594f9ff6f85f550d249ea9b48e40cddb29baa4
Макс АТС <abak.maks.frizoit@gmail.com>
2022-04-18 22:18:33 +0500

    Продуктивный день работы!

--- C.V.ResumeWPF/MainWindow.xaml.cs
+++ C.V.ResumeWPF/MainWindow.xaml.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using C.V.ResumeWPF.View;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -9,6 +10,7 @@ using System.Windows.Data;
 using System.Windows.Documents;
 using System.Windows.Input;
 using System.Windows.Media;
+using System.Windows.Media.Animation;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
@@ -20,9 +22,44 @@ namespace C.V.ResumeWPF
     /// </summary>
     public partial class MainWindow : Window
     {
+        List<Page> Pages = new List<Page>();
+        int index;
         public MainWindow()
         {
             InitializeComponent();
+            //this.Pages = new List<Page>();
+            //index = 0;
+            ////Перечень страниц
+            //Pages.Add(new OpenPage());
+        }
+        private void OpenningButton_Click(object sender, RoutedEventArgs e)
+        {
+            MainFrame.Content = (new OpenPage());
+        }
+
+        private void SubsequentButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.Content == null)
+            {
+                MainFrame.Content = Pages[index].Content;
+            }
+            else
+            {
+                index++;
+                if (index < Pages.Count)
+                {
+                    MainFrame.Content = Pages.ElementAt(index);
+                }
+            }
+        }
+
+        private void PreviousButton_Click(object sender, RoutedEventArgs e)
+        {
+            if (MainFrame.Content != null && index != 0)
+            {
+                index--;
+                MainFrame.Content = Pages[index];
+            }
         }
     }
 }
---
+++ C.V.ResumeWPF/Model/Core.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace C.V.ResumeWPF.Model
+{
+    internal class Core
+    {
+       public Entities context = new Entities();
+    }
+}
